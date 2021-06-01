e9aa028e984af35dd5abe9bb1d96362ad0ca5344
Júlio Dutra <jjcdutra2015>
2021-06-01 23:35:10 +0000

    Initial commit

---
+++ src/main/kotlin/br/com/zup/Application.kt
@@ -0,0 +1,21 @@
+package br.com.zup
+
+import io.micronaut.runtime.Micronaut.*
+import io.swagger.v3.oas.annotations.*
+import io.swagger.v3.oas.annotations.info.*
+
+@OpenAPIDefinition(
+    info = Info(
+            title = "keymanager-rest",
+            version = "0.0"
+    )
+)
+object Api {
+}
+fun main(args: Array<String>) {
+	build()
+	    .args(*args)
+		.packages("br.com.zup")
+		.start()
+}
+
